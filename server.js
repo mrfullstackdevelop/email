a9bbbbf8fbdadc2c8e32862b0b1d6b1e96d2ebb1
mrfullstackdevelop <74949313+mrfullstackdevelop@users.noreply.github.com>
2020-11-24 03:28:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
+const nodemailer = require("nodemailer")
 
 app.use(bodyParser.json())
 
 app.post("/send", async (req, res) => {
-	const { email, from, subject, content, dkim, selector } = req.body
-	nodemailer.createTransport({
-	    sendmail: true,
-	    newline: "unix",
-	    path: "/usr/sbin/sendmail",
-	    dkim: {
-	    	domainName: from.split("@").pop(),
-	    	keySelector: selector,
-	    	privateKey: dkim
-	    }
-	}).sendMail({
-	    from: from,
-	    to: email,
-	    subject: subject,
-	    html: content
-	}, (error, info) => {
-	    return res.status(200).json({
-	    	error: error ? error : false,
-	    	information: info
-	    })
-	})
+        const { email, from, subject, content, dkim, domain, selector } = req.body
+        nodemailer.createTransport({
+            sendmail: true,
+            newline: "unix",
+            path: "/usr/sbin/sendmail"
+        }).sendMail({
+            priority: "high",
+            encoding: "base64",
+            from: from,
+            to: email,
+            subject: subject,
+            html: content,
+            dkim: {
+                domainName: domain,
+                keySelector: selector.trim(),
+                privateKey: dkim.trim()
+            }
+        }, (error, info) => {
+            return res.status(200).json({
+                error: error ? error : false,
+                information: info
+            })
+        })
 })
 
 
